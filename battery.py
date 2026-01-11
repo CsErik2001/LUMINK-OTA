73cb587b8cdd5085099764ecf84bacba58cdb9fc
Erik <cserik2001@gmail.com>
2026-01-11 22:21:24 +0100

    1.8

--- battery.py
+++ battery.py
@@ -1,26 +1,22 @@
 from machine import ADC, Pin
 
-class Battery:
-    # Battery Specs:
-    # Model: 803450
-    # Type: Lithium battery
-    # Capacity: 2000mAh
-    # Rated Voltage: 3.7v
-    # Dimensions: 34x50x8mm
 
-    def __init__(self, pin_number, min_voltage=3.0, max_voltage=4.2, attenuation=ADC.ATTN_11DB, voltage_divider_factor=2.0):
+class Battery:
+    def __init__(self, pin_number, min_voltage=3.0, max_voltage=4.2, voltage_divider_factor=2.0):
         self.pin = ADC(Pin(pin_number))
-        self.pin.init(atten=attenuation)
+        self.pin.atten(ADC.ATTN_11DB)
         self.voltage_divider_factor = voltage_divider_factor
         self.min_voltage = min_voltage
         self.max_voltage = max_voltage
 
     def read_voltage(self):
-        raw_value = self.pin.read_u16()
-        voltage = (raw_value / 65535.0) * 3.3 * self.voltage_divider_factor
-        return voltage
+        microvolts = self.pin.read_uv()
+        volts_at_pin = microvolts / 1_000_000
+        real_voltage = volts_at_pin * self.voltage_divider_factor
+        return round(real_voltage, 2)
 
     def battery_percentage(self):
         voltage = self.read_voltage()
-        percentage = int(((voltage - self.min_voltage) / (self.max_voltage - self.min_voltage)) * 100)
-        return max(0, min(100, percentage))
+        pct = (voltage - self.min_voltage) / (self.max_voltage - self.min_voltage) * 100
+        pct = max(0, min(100, pct))
+        return int(pct)
